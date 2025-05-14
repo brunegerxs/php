a125a770077b7d1711b6f21b708d5925859e49a2
brunegerxs <bbaeznebiolo@escuelasproa.edu.ar>
2025-05-14 18:39:15 +0200

    Create .php

---
+++ .php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+$host = "localhost";
+$usuario = "root";
+$contrasena = "";
+$base_datos = "mi_basedatos";
+
+$conn = new mysqli($host, $usuario, $contrasena, $base_datos);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa";
+?>
+
+</body>
+</html>
